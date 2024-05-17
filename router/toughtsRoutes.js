8d67ccef3e18b07de09a3cced47df19b2651e7f7
Pedro Guedes <pguedeslsilva@gmail.com>
2024-05-17 00:30:18 -0300

    feat: definir o middleware auth e criar as rotas do crud faltante

--- router/toughtsRoutes.js
+++ router/toughtsRoutes.js
@@ -3,7 +3,16 @@ const router  = express.Router() // Colocando todos os métodos do express para 
 // Controller
 const ToughtsController = require('../controllers/ToughtsController')
 
+// Helpers
+const checkAuth = require('../helpers/auth').checkAuth
+
 // Rotas
+router.get('/dashboard', checkAuth, ToughtsController.dashboard)
+router.post('/remove', checkAuth, ToughtsController.removeTought)
+router.get('/add', checkAuth, ToughtsController.createTought)
+router.post('/add', checkAuth, ToughtsController.createToughtSave)
+router.get('/edit/:id', checkAuth, ToughtsController.updateTought)
+router.post('/edit', checkAuth, ToughtsController.updateToughtSave)
 router.get('/', ToughtsController.showToughts)
 
 module.exports = router
